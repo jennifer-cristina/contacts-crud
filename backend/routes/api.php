b0b1c9353472848706b6c35a88f793d93a5adacc
Jennifer Cristina <cristin4.jennifer@gmail.com>
2023-05-14 23:05:47 -0300

    chore: remove comments

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,19 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::get('/contacts', [ContactController::class, 'index']);
 
@@ -24,7 +12,3 @@ Route::post('/contacts', [ContactController::class, 'store']);
 Route::put('/contacts/{id}', [ContactController::class, 'update']);
 
 Route::delete('/contacts/{id}', [ContactController::class, 'delete']);
-
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
